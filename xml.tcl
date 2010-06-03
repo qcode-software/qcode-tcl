b2edaf7f92e07cb6f4b7a35e41bc371e0225747e
david <david@6c631035-10fc-438b-ade0-1daf01a8587a>
2010-06-03 08:53:52 +0000

    Added xml2dict documentation

--- xml.tcl
+++ xml.tcl
@@ -106,3 +106,56 @@ proc qc::xml2dict { xml root_element } {
     }
     return $dict
 }
+
+doc xml2dict {
+    Description {
+        Converts an XML structure in the form of a text string into a dict.
+        Will start parsing from the element that matches the rootElement. 
+        If nothing matches the root element an error will be thrown.
+
+        Should only be used when the XML structure will not have repeating elements since it will only return the contents of one of the repeating elements.
+        Does not look at attributes.
+    }
+    Usage {xml2dict $xml rootElement}
+    Examples {
+        % set xml {
+        <?xml version="1.0"?>
+            <messages>
+   	    <note>
+                <to>Bill</to>
+                <from>Ben</from>
+                <body>Information</body>
+            </note>
+        </messages>
+        }
+
+        % xml2dict $xml messages
+        note {to Bill from Ben body Information}
+        % xml2dict $xml note
+        to Bill from Ben body Information
+        % xml2dict $xml dfgdfg
+        XML parse error
+    
+
+        % set xml {
+        <?xml version="1.0"?>
+        <messages>
+            <note>
+                <to>Bill</to>
+                <from>Ben</from>
+                <body type="text">Information</body>
+            </note>
+            <note>
+                <to>Ben</to>
+                <from>Bill</from>
+                <body>What</body>
+            </note>
+        </messages>
+        }
+
+        % xml2dict $xml note
+        to Bill from Ben body Information
+    }
+}
+
+
