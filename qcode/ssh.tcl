ab9b04767182ba717231913bd100725530e7057a
daniel <daniel@6c631035-10fc-438b-ade0-1daf01a8587a>
2012-01-30 12:56:28 +0000

    make package. new ssh procs

---
+++ qcode/ssh.tcl
@@ -0,0 +1,36 @@
+package provide qcode 1.3
+package require doc
+namespace eval qc {}
+
+proc qc::ssh {args} {
+    set args [linsert $args 0 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -q]
+    return [exec -ignorestderr ssh {*}$args]
+}
+
+proc qc::scp {args} {
+    set args [linsert $args 0 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -q]
+    return [exec -ignorestderr scp {*}$args]
+}
+
+proc qc::ssh_call_proc {args} {
+    #| Take a proc definition and args and write a Tcl script that can be run on the remote host
+    # eg. ssh_call_proc user@remote link_checker /var/www/www.example.com
+ 
+    if {![regexp {^([^@]+)@([^@]+)$} [lindex $args 0] -> username host] } {
+	error "Usage ssh_call_proc user@host proc_name args"
+    }
+    qc::lshift args
+    set proc_name [qc::lshift args]
+    set script [qc::info_proc $proc_name]
+    append script \n "puts \[$proc_name $args\]"
+    set filename [qc::file_temp $script]
+    try {
+	scp $filename ${username}@${host}:$filename
+	set out [ssh ${username}@${host} /usr/bin/tclsh8.5 $filename]
+	ssh ${username}@${host} rm $filename    
+    } {
+	ssh ${username}@${host} rm -f $filename    
+    }
+    file delete $filename
+    return $out
+}
