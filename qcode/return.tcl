d78237011ae66cf203371124cf33283c0cf54d78
daniel <daniel@6c631035-10fc-438b-ade0-1daf01a8587a>
2012-05-15 12:10:13 +0000

    use content-disposition header to fix problem with csv downloads asking for username and password

--- qcode/return.tcl
+++ qcode/return.tcl
@@ -43,7 +43,7 @@ proc qc::return_xml { xml } {
 }
 
 proc qc::return_csv { text } { 
-    return2client content-type "text/csv; charset=utf-8" text $text
+    return2client content-type "text/csv; charset=utf-8" text $text Content-disposition "attachment;"
 }
 
 proc qc::return_soap { string } { 
