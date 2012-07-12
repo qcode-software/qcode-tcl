fae464e8becccf1af738871eacfe8c6de4d847eb
bernhard <bernhard@6c631035-10fc-438b-ade0-1daf01a8587a>
2012-07-12 11:28:17 +0000

    schema_update tool

---
+++ qcode/schema.tcl
@@ -0,0 +1,28 @@
+package provide qcode 1.6
+package require doc
+namespace eval qc {
+    namespace export qc *
+}
+
+proc qc::schema_update {version code} {
+    #| Run the code if it applies to the current schema in order to bring the schema up to the next version.
+    db_trans {
+	db_1row {select version as current_version from schema for update}
+	if { $current_version == $version } {
+	    log "Updating schema version $version ..."
+	    uplevel 1 $code
+	    incr version
+	    db_dml {update schema set version=:version}
+	    log "Schema updated to version $version"
+	}
+    }
+}
+
+doc qc::schema_update {
+    Examples {
+	schema_update 19 {
+	    db_dml { alter table product add column ean bigint }
+	}
+	
+    }
+}
