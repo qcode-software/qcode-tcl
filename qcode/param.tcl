aa18e2da868293e80b25cdb9f362b5a4595c4e51
david <david@6c631035-10fc-438b-ade0-1daf01a8587a>
2012-08-28 16:32:00 +0000

    EoD in progress

--- qcode/param.tcl
+++ qcode/param.tcl
@@ -3,12 +3,22 @@ package require doc
 namespace eval qc {}
 
 proc qc::param { param_name } {
-    if { [ne [set param_value [ns_config ns/server/[ns_info server] $param_name]] ""] } {
+    global $param_name
+    if { [info exists $param_name] } {
+        # global variable
+        return [set $param_name]
+    } elseif { [info commands ns_config] eq "ns_config" && [ne [set param_value [ns_config ns/server/[ns_info server] $param_name]] ""] } {
+        # Aolserver param
 	return $param_value
+    } elseif { [info commands ns_db] eq "ns_db" } {
+        # DB param
+        set qry {select param_value from param where param_name=:param_name}
+        db_cache_1row -ttl 86400 $qry
+        return $param_value
+    } else {
+        # I give up
+        error "I don't know how to find param $param_name"
     }
-    set qry {select param_value from param where param_name=:param_name}
-    db_cache_1row -ttl 86400 $qry
-    return $param_value
 }
 
 
