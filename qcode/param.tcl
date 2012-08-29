056058c87883d7ff1676939388063209648a25cf
david <david@6c631035-10fc-438b-ade0-1daf01a8587a>
2012-08-29 10:36:37 +0000

    param_exists updates

--- qcode/param.tcl
+++ qcode/param.tcl
@@ -3,6 +3,9 @@ package require doc
 namespace eval qc {}
 
 proc qc::param { param_name } {
+    #| Return param value.
+    #| First checks if param_name is a global variable (as used my muppet)
+    #| then tries nsd params and DB.
     global $param_name
     if { [info exists $param_name] } {
         # global variable
@@ -23,13 +26,23 @@ proc qc::param { param_name } {
 
 
 proc qc::param_exists { param_name } {
-    if { [ne [ns_config ns/server/[ns_info server] $param_name] ""] } {
+    #| Check for param existence.
+    #| First checks if param_name is a global variable (as used my muppet)
+    #| then tries nsd params and DB.
+    global $param_name
+    if { [info exists $param_name] && [set $param_name] ne ""} {
 	return true
     }
-    set qry {select param_value from param where param_name=:param_name}
-    db_cache_0or1row -ttl 86400 $qry {
-	return false
-    } {
+    if { [info commands ns_config] eq "ns_config" && [ne [ns_config ns/server/[ns_info server] $param_name] ""] } {
 	return true
     }
+    if { [info commands ns_db] eq "ns_db" } {
+        set qry {select param_value from param where param_name=:param_name}
+        db_cache_0or1row -ttl 86400 $qry {
+	    return false
+        } {
+	    return true
+        }
+    }
+    return false
 }
