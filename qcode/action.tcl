d0236c7bea41d34bbe14c7e579e7493db1cefed4
david <david@6c631035-10fc-438b-ade0-1daf01a8587a>
2012-09-07 08:56:14 +0000

    package provide 1.8

--- qcode/action.tcl
+++ qcode/action.tcl
@@ -1,4 +1,4 @@
-package provide qcode 1.7
+package provide qcode 1.8
 namespace eval qc {}
 proc qc::action { description url {actionKey ""} } {
     if { [eq "" $actionKey] } {
--- qcode/sql_where_in.tcl
+++ qcode/sql_where_in.tcl
@@ -1,4 +1,4 @@
-package provide qcode 1.7
+package provide qcode 1.8
 package require doc
 namespace eval qc {}
 proc qc::sql_where_in {column list {default false} } {
