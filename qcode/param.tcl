e505e34e3a282e01c298374e890574481b17043c
david <david@6c631035-10fc-438b-ade0-1daf01a8587a>
2012-09-12 14:53:07 +0000

    param: continue if not found in DB to be consistant and param_exists update

--- qcode/param.tcl
+++ qcode/param.tcl
@@ -8,27 +8,30 @@ proc qc::param { param_name } {
     #| then tries nsd params and DB.
     if { [info exists param::${param_name}] } {
         return [set param::${param_name}]
-    } elseif { [info commands ns_config] eq "ns_config" && [ne [set param_value [ns_config ns/server/[ns_info server] $param_name]] ""] } {
+    } 
+    if { [info commands ns_config] eq "ns_config" && [ne [set param_value [ns_config ns/server/[ns_info server] $param_name]] ""] } {
         # Aolserver param
 	return $param_value
-    } elseif { [info commands ns_db] eq "ns_db" } {
+    } 
+    if { [info commands ns_db] eq "ns_db" } {
         # DB param
         set qry {select param_value from param where param_name=:param_name}
-        db_cache_1row -ttl 86400 $qry
-        return $param_value
-    } else {
-        # I give up
-        error "I don't know how to find param $param_name"
-    }
+        db_cache_0or1row -ttl 86400 $qry {
+            # Not found in DB
+        } {
+            return $param_value
+        }
+    } 
+    # I give up
+    error "I don't know how to find param $param_name"
 }
 
 
 proc qc::param_exists { param_name } {
     #| Check for param existence.
-    #| First checks if param_name is a global variable (as used my muppet)
+    #| First checks if param_name exists as a variable in param:: namespace (as used by muppet)
     #| then tries nsd params and DB.
-    global $param_name
-    if { [info exists $param_name] && [set $param_name] ne ""} {
+    if { [info exists param::${param_name}] && [set param::${param_name}] ne ""} {
 	return true
     }
     if { [info commands ns_config] eq "ns_config" && [ne [ns_config ns/server/[ns_info server] $param_name] ""] } {
