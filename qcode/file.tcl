03f21359e4e2bb2fbec805d41de4eb7f47a3fd7c
Peter Chaplin <peter@qcode.co.uk>
2013-02-25 09:42:26 +0000

    New version

--- qcode/file.tcl
+++ qcode/file.tcl
@@ -1,4 +1,4 @@
-package provide qcode 1.16
+package provide qcode 1.17
 package require doc
 namespace eval qc {}
 proc qc::file_temp {text {mode 0600}} {
--- qcode/perl.tcl
+++ qcode/perl.tcl
@@ -1,4 +1,4 @@
-package provide qcode 1.15
+package provide qcode 1.17
 package require doc
 namespace eval qc {}
 
