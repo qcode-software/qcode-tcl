0a2a3b98a33f0c9bcdbf01b95c305a41af633aed
Peter Chaplin <peter@qcode.co.uk>
2013-08-05 09:44:50 +0100

    Update tests

--- qcode/html_table_scroll.tcl
+++ qcode/html_table_scroll.tcl
@@ -11,7 +11,7 @@ proc qc::html_table_scroll {args} {
     set div_style ""
     set div_class [list "scroll"]
     if { [lower $height] eq "max" } {
-	lappend div_class dynamicResize
+	lappend div_class "maximize-height"
     } else {
 	set div_style [style_set $div_style height ${height}px]
     }
