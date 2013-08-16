63b80b18a61abdf1cb0bb59d7703021ba99d6775
Peter Chaplin <peter@qcode.co.uk>
2013-08-16 11:03:08 +0100

    Use expiration epoch instead of interval, some minor tweaks

--- qcode/authorise.tcl
+++ qcode/authorise.tcl
@@ -9,46 +9,55 @@ doc authorisation {
 
 proc qc::authorise_token_create {args} {
     #| Create an authorisation token for this url
-    dict2vars $args expires employee_id referrer target 
+    args2vars $args expires employee_id referrer target 
     default expires "24 hours"
     default employee_id [auth]
     default referrer [conn_url]
     if { ![regexp {^https?://} $target] } {
         set target "[qc::conn_location]/[string trimleft $target /]"
     }
-    set interval [expr {[clock scan $expires] - [clock seconds]}]
-    set timestamp [expr {[clock seconds] / $interval}]
+    set expiration_epoch [clock scan $expires]
     db_1row {
         select 
-        sha1(concat(authorisation_key,:target,:referrer,:timestamp)) as hash
+        sha1(concat(authorisation_key,:target,:referrer,:expiration_epoch)) as hash
         from employee
         where employee_id=:employee_id
     }
-    return "$employee_id $interval $hash"
+    return "$employee_id $expiration_epoch $hash"
 }
 
 doc qc::authorise_token_create {
     Parent authorisation
 }
 
-proc qc::authorise_token_check {args} {
+proc qc::authorise_token {} {
     #| Check the authorisation token of the current request
     #| and return the employee_id if authorised
     if { ![form_var_exists authorisation_token] } {
         error "Authorisation token missing" {} AUTHORISATION
     }
     set token [form_var_get authorisation_token]
-    lassign $token employee_id interval hash
+    lassign $token employee_id expiration_epoch hash
     check employee_id INT
-    check interval INT
-    # referer
-    set referrer [ns_set get [ns_conn headers] Referer]
+    check expiration_epoch INT
+
+    if { $expiration_epoch < [clock seconds] } {
+        error "Authorisation failed. Token has expired." {} AUTHORISATION
+    }
+
+    # referrer
+    set header_set_id [ns_conn headers]
+    if { [ns_set find $header_set_id Referer] == -1 } {
+        set referrer ""
+    } else {
+        set referrer [url_root [ns_set get [ns_conn headers] Referer]]
+    }
+
     set target [conn_url]
-    set timestamp [expr {[clock seconds] / $interval}]
    
     set qry {
         select 
-        sha1(concat(authorisation_key,:target,:referrer,:timestamp)) as check_hash
+        sha1(concat(authorisation_key,:target,:referrer,:expiration_epoch)) as check_hash
         from employee
         where employee_id=:employee_id
     }
@@ -63,6 +72,6 @@ proc qc::authorise_token_check {args} {
     }
 }
 
-doc qc::authorise_token_check {
+doc qc::authorise_token {
     Parent authorisation
 }
