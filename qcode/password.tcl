cc94fc43e1816bb6a3f00ff853230f1725492a1d
Daniel Clark <daniel@qcode.co.uk>
2013-08-29 15:39:53 +0100

    split up functionality of password_complexity_check

--- qcode/password.tcl
+++ qcode/password.tcl
@@ -10,6 +10,41 @@ proc qc::password_hash { password {strength 7} } {
     return $password_hash
 }
 
+proc qc::password_complexity_ok { args } {
+    #| Return true if password meets min, max and minclasses complexity requirements
+    #| Otherwise return false
+    #| max size is limited to 72
+    #| Usage: password_complexity_check password min 2 max 10 minclasses 4
+    qc::args $args password args
+    qc::args2vars $args min max minclasses
+        
+    # Check min password length
+    if { [info exists min] && [string length $password] < $min } {
+        return false
+    }
+    # Check max password length
+    if { [info exists max] && [string length $password] > $max } {
+        return false
+    }
+    # Check password contains minimum number of character classes: upper, lower, digit, punctuation
+    if { [info exists minclasses] } {
+        foreach class [list upper lower digit] {
+            if { [regexp "\[\[:${class}:\]\]" $password] } {
+                incr classes
+            }
+        }
+        # [[:punct:]] matches !"%&*()_-{}][@:#';?/.,\
+        # Using inverse instead to catch £$^+=~><|`¬ in addition
+        if {[regexp {[^[:upper:][:lower:][:digit:]]} $password]} {
+            incr classes
+        }
+        if { $classes < $minclasses } {
+            return false
+        }
+    }
+    return true
+}
+
 proc qc::password_complexity_check { args } {
     #| Checks password against min, max and minclasses complexity requirements
     #| max size is limited to 72
@@ -21,24 +56,16 @@ proc qc::password_complexity_check { args } {
     default max 72
     default minclasses 1 
     
-    # Check password length and that confirm_password matches
-    if { [string length $password] < $min } {
+    # Check min password length
+    if { ! [qc::password_complexity_ok $password min $min] } {
         error "Your password must be at least $min characters long" {} USER
     }
-    if { [string length $password] > $max } {
+    # Check max password length
+    if { ! [qc::password_complexity_ok $password max $max] } {
         error "Your password must be less than $max characters long" {} USER
     }
-    foreach class [list upper lower digit] {
-        if { [regexp "\[\[:${class}:\]\]" $password] } {
-            incr classes
-        }
-    }
-    # [[:punct:]] matches !"%&*()_-{}][@:#';?/.,\
-    # Using inverse instead to catch £$^+=~><|`¬ in addition
-    if {[regexp {[^[:upper:][:lower:][:digit:]]} $password]} {
-        incr classes
-    }
-    if { $classes < $minclasses } {
+    # Check password contains minimum number of character classes: upper, lower, digit, punctuation
+    if { ! [qc::password_complexity_ok $password minclasses $minclasses] } {
         error "Your password must contain at least $minclasses of uppercase, lowercase, numeric or punctuation" {} USER
     }
     return true
