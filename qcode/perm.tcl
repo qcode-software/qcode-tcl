9bb747c6060646f49497ef7800fda42ddc7fa0a7
Daniel Clark <daniel@qcode.co.uk>
2013-10-28 17:01:17 +0000

    name space perm_set. Remove deprecated procs

--- qcode/perm.tcl
+++ qcode/perm.tcl
@@ -2,7 +2,7 @@ package provide qcode 2.0
 package require doc
 namespace eval qc {}
 
-proc perm_set {employee_id perm_name args} {
+proc qc::perm_set {employee_id perm_name args} {
     #| Configure employee permissions
     #| Usage: perm_set employee_id perm_name ?method? ?method?
     set methods [string toupper $args]
@@ -95,107 +95,4 @@ proc qc::perm_if {perm_name method if_code {. else} {else_code ""} } {
     } elseif {[ne $else_code ""]} {
 	uplevel 1 $else_code
     }
-}
-
-proc qc::perm_get { perm_name property } {
-    # Deprecate
-    # TODO
-    #| Abstraction layer for accessing properties
-    set qry { select * from perm where perm_name=:perm_name }
-    db_cache_1row $qry
-    return [set $property]
-}
-
-proc qc::perm_string_test { employee_id m perm_string } {
-    # Deprecate
-    # TODO
-    #| tests for method $m using all parties listed in $parties
-    #| against the list of permissions in $perm_list
-    #| return boolean 1 when permission is granted
-    if { [dict exists $perm_string $employee_id] && [string first $m [dict get $perm_string $employee_id]]!=-1 } {
-	return 1
-    } else {
-	return 0
-    }
-}
-
-proc qc::perm_method_abbrev { method } {
-    # Deprecate
-    # TODO
-    #| Return the single letter abbreviation for $method
-    switch -glob -- $method {
-	append { return a }
-	read { return r }
-	write { return w }
-    }
-    error "Unknown permission type $method"
-}
-
-proc qc::perm_method_long { m } {
-    # Deprecate
-    # TODO
-    #| Return long name for single letter abbreviation
-    switch $m {
-	a { return create }
-	r { return view }
-	w { return edit }
-    }
-    error "Unknown permission type $m"
-}
-
-proc qc::perm_method_description { m } {
-    # DEPRECATE
-    # TODO
-    #| Return a simple description of the method
-    switch $m {
-	a { return Create }
-	r { return View }
-	w { return Edit }
-    }
-    error "Unknown permission type $m"
-}
-
-proc qc::perm_string_add { perm_string employee_id m } {
-    # DEPRECATE
-    # TODO
-    #| Add method $m for $employee_id to $perm_string
-    #| and return new perm_string
-    if { [dict exists $perm_string $employee_id] } {
-	set methods [dict get $perm_string $employee_id]
-	if { [string first $m $methods]==-1 } {
-	    # Add the method
-	    dict set perm_string $employee_id "${methods}$m"
-	}
-    } else {
-	lappend perm_string $employee_id $m
-    }
-    return $perm_string
-}
-
-proc qc::perm_string_remove { perm_string employee_id m } {
-    # DEPRECATE
-    # TODO
-    #| Remove method $m for $employee_id to $perm_string
-    #| and return new perm_string
-    if { [dict exists $perm_string $employee_id] } {
-	set methods [dict get $perm_string $employee_id]
-	if { [string first $m $methods]!=-1 } {
-	    # Remove the method
-	    if { [string length $methods]==1 } {
-		dict unset perm_string $employee_id
-	    } else {
-		dict set perm_string $employee_id [regsub $m $methods {}]
-	    }
-	}
-    }
-    return $perm_string
-}
-
-proc qc::perm_list_employees { perm_name method } {
-    # DEPRECATE
-    # TODO
-    #| List all employee_ids (excluding charlie root) who have $method permission on $perm_name
-    set perm_string [perm_get $perm_name perm_string]
-    set m [perm_method_abbrev $method]
-    return [lexclude [dict keys [dict filter $perm_string value *${m}*]] 0]
 }
