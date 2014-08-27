ac3bf6ad44312c2f2522c976e4e4932610ea5a65
Peter Chaplin <peter@qcode.co.uk>
2014-08-27 09:57:47 +0100

    Remove type from the image table

--- tcl/db_file.tcl
+++ tcl/db_file.tcl
@@ -153,12 +153,12 @@ proc qc::plupload.html {name chunk chunks file filename {mime_type ""}} {
 	set file_id [qc::db_file_insert {*}$flags $tmp_file]
         if { [param_exists image_db_table] && [qc::file_is_valid_image $tmp_file] } {
             set image_table [param_get image_db_table]
-            dict2vars [qc::image_file_info $tmp_file] width height type
+            dict2vars [qc::image_file_info $tmp_file] width height
             db_dml {
                 insert into \"[string map {\" \"\"} $image_table]\"
-                (file_id, width, height, type)
+                (file_id, width, height)
                 values
-                (:file_id, :width, :height, :type)
+                (:file_id, :width, :height)
             }
         }
         file delete $tmp_file
