977821f23ed27c5b6f50d32e6750870d6b3bedf4
Nicky Johnstone <nicky@qcode.co.uk>
2015-01-14 15:24:05 +0000

    Migrated cast, castable, and is ensembles from blog.

---
+++ tcl/castable.tcl
@@ -0,0 +1,137 @@
+namespace eval qc::castable {
+    
+    namespace export integer bigint smallint decimal boolean timestamp timestamptz char varchar enumeration text domain safe_html safe_markdown
+    namespace ensemble create -unknown {
+        data_type_parser
+    }
+
+    proc integer {string} {
+        #| Test if the given string can be cast to an integer.
+        try {
+            qc::cast integer $string
+            return true
+        } on error [list error_message options] {
+            return false
+        }
+    }
+
+    proc bigint {string} {
+        #| Test if the given string can be cast to a big integer.
+        try {
+            qc::cast bigint $string
+            return true
+        } on error [list error_message options] {
+            return false
+        }
+    }
+
+    proc smallint {string} {
+        #| Test if the given string can be cast to a small integer.
+        try {
+            qc::cast smallint $string
+            return true
+        } on error [list error_message options] {
+            return false
+        }
+    }
+
+    proc decimal {string {precision ""}} {
+        #| Test if the given string can be cast to a decimal.
+        try {
+            qc::cast decimal $string
+            return true
+        } on error [list error_message options] {
+            return false
+        }
+    }
+
+    proc boolean { string {true t} {false f} } {
+        #| Test if the given string can be cast to an boolean.
+        try {
+            qc::cast boolean $string
+            return true
+        } on error [list error_message options] {
+            return false
+        }        
+    }
+
+    proc timestamp {string} {
+        #| Test if the given string can be cast to a timestamp without timezone.
+        try {
+            qc::cast timestamp $string
+            return true
+        } on error [list error_message options] {
+            return false
+        }        
+    }
+
+    proc timestamptz {string} {
+        #| Test if the given string can be cast to an timestamp with timezone.
+        try {
+            qc::cast timestamptz $string
+            return true
+        } on error [list error_message options] {
+            return false
+        }        
+    }
+
+    proc char {length string} {
+        #| Test if the given string can be cast to a fixed length string.
+        try {
+            qc::cast char $length $string
+            return true
+        } on error [list error_message options] {
+            return false
+        }        
+    }
+
+    proc varchar {length string} {
+        #| Test if the given string can be cast to a varchar of given length.
+        try {
+            qc::cast varchar $length $string
+            return true
+        } on error [list error_message options] {
+            return false
+        }        
+    }
+
+    proc enumeration {name value} {
+        #| Test if the given value can be cast to an enumeration value in $name.
+        try {
+            qc::cast enumeration $name $value
+            return true
+        } on error [list error_message options] {
+            return false
+        }
+    }
+
+    proc text {string} {
+        #| Test if the given string can be cast to text.
+        try {
+            qc::cast text $string
+            return true
+        } on error [list error_message options] {
+            return false
+        }
+    }
+
+    proc domain {domain_name value} {
+        #| Test if the given value can be cast to domain $domain_name.
+        try {
+            qc::cast domain $domain_name $value
+            return true
+        } on error [list error_message options] {
+            return false
+        }
+    }
+
+    proc safe_html {text} {
+        #| Test if the given text can be cast to safe html.
+        return [qc::is safe_html $text]
+    }
+
+    proc safe_markdown {text} {
+        #| Test if the given text can be case to safe markdown.
+        return [qc::is safe_markdown $text]
+    }
+}
