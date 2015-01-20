ee803adecb104e9e42e5f426dd2c5afe021cf279
Nicky Johnstone <nicky@qcode.co.uk>
2015-01-20 09:57:14 +0000

    fix

--- tcl/check.tcl
+++ tcl/check.tcl
@@ -129,7 +129,7 @@ proc qc::check {args} {
 	# Check
 	if {!([info exists type_args($TYPE)] && [info commands "::qc::is_$type"] ne "" && [qc::is_$type $varValue {*}$type_args($TYPE)])
 	    && 
-	    !(![info exists type_args($TYPE)] && [info commands "::qc::is::$type"] ne "" && [qc::is $type $varValue])
+	    !(![info exists type_args($TYPE)] && [info commands "::qc::is_$type"] ne "" && [qc::is_$type $varValue])
             &&
             !([info exists type_args($TYPE)] && [info commands "::qc::is_$type"] eq "" && [is_$type $varValue {*}$type_args($TYPE)])
 	    && 
