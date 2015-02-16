a88644330cac58ba01068919fb4e02f41a1205eb
Daniel Clark <daniel@qcode.co.uk>
2015-02-16 15:40:11 +0000

    fastpath gzip filter. filter to set expire headers

---
+++ tcl/filter.tcl
@@ -0,0 +1,27 @@
+namespace eval qc {
+    namespace export filter_fastpath_gzip filter_set_expires
+}
+
+proc qc::filter_fastpath_gzip {filter_when file_extensions} {
+    #| Postauth filter to seed filesystem with .gz versions of static files if the client accepts gzip-ed content.
+    set conn_path [qc::conn_path]
+    set file_path [ns_pagepath]$conn_path
+    set gzipped_file_path [ns_pagepath]${conn_path}.gz
+    
+    if { [ns_conn zipaccepted] && [file readable $file_path] && [file extension $file_path] in $file_extensions && [file type $file_path] in [list "link" "file"] && ![file readable $gzipped_file_path]} {
+        ns_gzipfile $file_path $gzipped_file_path
+    }
+    return "filter_ok"
+}
+
+proc qc::filter_set_expires {filter_when seconds {cache_response_directive ""}} {
+    #| Postauth filter to set cache control headers: Expires, & Cache-Control. 
+    #| If "cache_response_directive" is specified the function adds the "max-age" header field to the response "Cache-Control" header.
+    # cache_response_directive: public, private, no-cache, no-store, no-transform, must-revalidate, or proxy-revalidate
+    if { $cache_response_directive ne "" } {
+        ns_setexpires -cache-control $cache_response_directive $seconds
+    } else {
+        ns_setexpires $seconds
+    }
+    return "filter_ok"
+}
