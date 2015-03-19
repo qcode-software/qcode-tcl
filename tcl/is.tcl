605cdc22b1f34c84c12b7f71b6eb2f9aa19f2f4b
Peter Chaplin <peter@qcode.co.uk>
2015-03-19 09:16:03 +0000

    Extend is_url to allow empty components

--- tcl/is.tcl
+++ tcl/is.tcl
@@ -595,11 +595,11 @@ namespace eval qc::is {
         if { $relative } {
             return [regexp -expanded {
                 # path
-                ^([a-zA-Z0-9_\-\.~+/%]+)?
+                ^([a-zA-Z0-9_\-\.~+/%]*)?
                 # query
-                (\?[a-zA-Z0-9_\-\.~+/%=&]+)?
+                (\?[a-zA-Z0-9_\-\.~+/%=&]*)?
                 # anchor
-                (\#[a-zA-Z0-9_\-\.~+/%]+)?
+                (\#[a-zA-Z0-9_\-\.~+/%]*)?
                 $
             } $url]
         } else {
@@ -611,11 +611,11 @@ namespace eval qc::is {
                 # port
                 (:[0-9]+)?
                 # path
-                ([a-zA-Z0-9_\-\.~+/%]+)?
+                ([a-zA-Z0-9_\-\.~+/%]*)?
                 # query
-                (\?[a-zA-Z0-9_\-\.~+/%=&]+)?
+                (\?[a-zA-Z0-9_\-\.~+/%=&]*)?
                 # anchor
-                (\#[a-zA-Z0-9_\-\.~+/%]+)?
+                (\#[a-zA-Z0-9_\-\.~+/%]*)?
                 $
             } $url]
         }
