e67706a3668007d4fca1f5d9cff44cde78ffe5a1
Nicky Johnstone <nicky@qcode.co.uk>
2015-06-16 10:58:07 +0100

    bug fix; refine

--- tcl/http.tcl
+++ tcl/http.tcl
@@ -492,7 +492,7 @@ proc qc::http_header_get {header_name} {
         
         return [join $header_values ","]
     } else {
-        return [ns_set value $headers_set_id $header_name]
+        return [ns_set iget $headers_set_id $header_name]
     }
 }
 
@@ -606,14 +606,18 @@ proc qc::http_header_sort_values_by_weight {values} {
     return $sorted
 }
 
-proc qc::http_header_best_mime_type {values available_media} {
+proc qc::http_header_best_mime_type {header_name available_media} {
     #| Returns a mime type from the available media list that is the best match
-    #| for the preferences of the header values given.
+    #| for the preferences of the header.
     #| If any mime type will suffice then "*/*" is returned.
-    #| Returns the empty string if no match is found.
+    #| Returns the empty string if no sufficient match is found.
     
-    # TODO sort values by specificity before weight
-    set sorted [qc::http_header_sort_values_by_weight $values]   
+    # TODO sort by specificity before weight
+    set header_value [qc::http_header_get $header_name]
+    if { $header_value eq "" } {
+        return -code error "Header \"$header_name\" doesn't exist."
+    }
+    set sorted [qc::http_header_sort_values_by_weight [qc::http_header_parse $header_name $header_value]]   
     foreach dict $sorted {
         lassign [split [dict get $dict token] "/"] type subtype
         # If best mime type is "*/*" then let caller know so that they can choose the most suitable type.
