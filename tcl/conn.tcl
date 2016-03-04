e68c2ce33986c24d8c20532f45157785419d4ff9
Priyank Jalu <priyank@qcode.co.uk>
2016-03-04 11:05:32 +0000

    Moved qc::handler_* procs into their own file

--- tcl/conn.tcl
+++ tcl/conn.tcl
@@ -1,5 +1,5 @@
 namespace eval qc {
-    namespace export conn_* handler_restful handler_files handler_db_files
+    namespace export conn_*
 }
 
 proc qc::conn_remote_ip {} {
@@ -189,45 +189,4 @@ proc qc::conn_method {} {
         set method [ns_conn method]
     }
     return $method
-}
-
-proc qc::handler_restful {} {
-    #| Handler for registered restful URL handlers.
-    set url_path [qc::conn_path]
-    set method [qc::conn_method]
-    
-    if {[qc::handlers exists $method $url_path]} {
-        set result [qc::handlers call $method $url_path]
-        # Check if conn is still waiting to be served
-        if { ![qc::conn_served] } {
-            # If a non-GET method then return the global data structure otherwise return the result as text/html.
-            if {$method ne "GET"} {
-                return [qc::return_response]
-            } else {
-                return [ns_return 200 text/html $result]
-            }
-        }
-    }
-}
-
-proc qc::handler_files {} {
-    #| Handler for file requests that haven't been resolved by fastpath.
-    set url_path [qc::conn_path]
-    if { [file exists [ns_pagepath]$url_path] } {
-        # static file
-        ns_register_fastpath GET $url_path
-        ns_register_fastpath HEAD $url_path
-        ns_returnfile 200 [ns_guesstype [ns_pagepath]$url_path] [ns_pagepath]$url_path
-    }
-}
-
-proc qc::handler_db_files {} {
-    #| Handle requests for files stored in the database that have yet to be cached on disk
-    set url_path [qc::conn_path]
-    if { [regexp {^/image/} $url_path] } {
-        # images
-        qc::image_handler [ns_pagepath]/image
-    } elseif { [regexp {^/file/} $url_path] } {
-        qc::file_handler [ns_pagepath]/file
-    }
 }
