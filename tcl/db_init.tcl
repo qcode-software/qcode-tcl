0ec237964f09314a1d519d75f59c50ea39187299
Gordon <gordon@qcode.co.uk>
2016-08-09 17:08:57 +0100

    added code to create pgcrypto extension if it doesn't already exist if the user is a superuser

--- tcl/db_init.tcl
+++ tcl/db_init.tcl
@@ -93,6 +93,17 @@ proc qc::db_init {} {
 	}
     }
 
+    # Make sure pgcrypto extension exists
+    if {![qc::db_extension_exists pgcrypto]} {
+	if { [qc::db_user_is_superuser [qc::db_user]]} {
+	    db_dml {
+		CREATE EXTENSION pgcrypto;
+	    }
+	} else {
+	    error "Database user \"[qc::db_user]\" is not superuser so pgcrypto extension cannot be created"
+	}
+    }
+    
     # pgcryto
     if {[qc::db_extension_exists pgcrypto]} {
 	db_dml {
