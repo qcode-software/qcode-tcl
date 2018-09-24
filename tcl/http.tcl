e5ca0fa3ea21bc9b96205f6430053bdef15a5d20
David Osborne <david@qcode.co.uk>
2018-09-24 16:41:07 +0100

    http procs

--- tcl/http.tcl
+++ tcl/http.tcl
@@ -57,13 +57,13 @@ proc qc::http_curl {args} {
 	    # default timeout 60seconds
 	    set timeout 60000
         }
-        qc::try {
+        ::try {
 	    set dict [ns_proxy eval $handle $script $timeout]
 	    ns_proxy release $handle
 	    return $dict
-        } {
+        } on error {error_message options} {
 	    ns_proxy release $handle
-	    error $::errorMessage $::errorInfo $::errorCode
+	    error $error_message [dict get $options -errorinfo] [dict get $options -errorcode]
         }
     } else {
         # no ns_proxy so evaluate directly
@@ -74,8 +74,22 @@ proc qc::http_curl {args} {
 proc qc::http_post {args} {
     #| Perform an HTTP POST
     # args is name value name value ... list
-    # usage http_post ?-noproxy? ?-timeout timeout? ?-encoding encoding? ?-content-type content-type? ?-soapaction soapaction? ?-accept accept? ?-authorization authorization? ?-data data? ?-valid_response_codes? ?-headers {name value name value ...}? url ?name value? ?name value?
-    args $args -noproxy -timeout 60 -sslversion tlsv1 -encoding utf-8 -content-type ? -soapaction ? -accept ? -authorization ? -headers {} -data ? -valid_response_codes {100 200} url args
+    args \
+        $args \
+        -noproxy \
+        -timeout 60 \
+        -sslversion tlsv1 \
+        -encoding utf-8 \
+        -content-type ? \
+        -soapaction ? \
+        -accept ? \
+        -authorization ? \
+        -headers {} \
+        -data ? \
+        -filename ? \
+        -valid_response_codes [list 100 200] \
+        url \
+        args
 
     if { ![info exists data]} {
 	set pairs {}
@@ -105,7 +119,18 @@ proc qc::http_post {args} {
 	lappend httpheaders [qc::http_header $name $value]
     }
    
-    set curl_args [list {*}[qc::iif [info exists noproxy] {-proxy ""} {}] -headervar return_headers -url $url -sslverifypeer 0 -sslverifyhost 0 -timeout $timeout -sslversion $sslversion -bodyvar html -post 1 -httpheader $httpheaders]
+    set curl_args [list \
+                      {*}[qc::iif [info exists noproxy] {-proxy ""} {}] \
+                      -headervar return_headers \
+                      -url $url \
+                      -sslverifypeer 0 \
+                      -sslverifyhost 0 \
+                      -timeout $timeout \
+                      -sslversion $sslversion \
+                      -bodyvar html \
+                      -post 1 \
+                      -httpheader $httpheaders \
+                   ]
 
     if { [info exists content-type] && [string match "multipart/*" ${content-type}] } {
         # eg. multipart/formdata
@@ -116,6 +141,10 @@ proc qc::http_post {args} {
     } else {
         # eg. application/x-www-form-urlencoded
         lappend curl_args -postfields $data
+    }
+
+    if { [info exists filename] } {
+        lappend curl_args -file $filename
     }
 
     dict2vars [qc::http_curl {*}$curl_args] return_headers html responsecode curlErrorNumber
@@ -184,8 +213,23 @@ proc qc::http_header {name value} {
 }
 
 proc qc::http_put {args} {
-    # usage http_put ?-header 0? ?-timeout timeout? ?-infile infile? ?-data data? ?-headers {name value name value ...}? url
-    args $args -header 0 -timeout 60 -sslversion tlsv1 -headers {} -infile ? -data ? url 
+    # usage http_put \
+    #   ?-valid_response_codes 200? \
+    #   ?-header 0? \
+    #   ?-timeout timeout? \
+    #   ?-infile infile? \
+    #   ?-data data? \
+    #   ?-headers {name value name value ...}? \
+    #   url
+    args $args \
+        -header 0 \
+        -timeout 60 \
+        -valid_response_codes [list 200] \
+        -sslversion tlsv1 \
+        -headers {} \
+        -infile ? \
+        -data ? \
+        url 
 
     set httpheaders {}
     foreach {name value} $headers {
@@ -195,30 +239,88 @@ proc qc::http_put {args} {
     if { [info exists data] && [info exists infile]} {
         error "qc::http:put must have only 1 of -data or -infile specified"
     } elseif { [info exists infile] } {
-        dict2vars [qc::http_curl -header $header -upload 1 -infile $infile -headervar return_headers -url $url -sslverifypeer 0 -sslverifyhost 0 -timeout $timeout -sslversion $sslversion -followlocation 1 -httpheader $httpheaders  -bodyvar html] return_headers html responsecode curlErrorNumber
+        dict2vars \
+                [qc::http_curl \
+                    -header $header \
+                    -upload 1 \
+                    -infile $infile \
+                    -headervar return_headers \
+                    -url $url \
+                    -sslverifypeer 0 \
+                    -sslverifyhost 0 \
+                    -timeout $timeout \
+                    -sslversion $sslversion \
+                    -followlocation 1 \
+                    -httpheader $httpheaders \
+                    -bodyvar html \
+                ] \
+                return_headers \
+                html \
+                responsecode \
+                curlErrorNumber
+
     } elseif { [info exists data] }  {
-        dict2vars [qc::http_curl -header $header -customrequest PUT -postfields $data -headervar return_headers -url $url -sslverifypeer 0 -sslverifyhost 0 -timeout $timeout -sslversion $sslversion -followlocation 1 -httpheader $httpheaders  -bodyvar html] return_headers html responsecode curlErrorNumber
+        dict2vars \
+            [qc::http_curl \
+                -header $header \
+                -customrequest PUT \
+                -postfields $data \
+                -headervar return_headers \
+                -url $url \
+                -sslverifypeer 0 \
+                -sslverifyhost 0 \
+                -timeout $timeout \
+                -sslversion $sslversion \
+                -followlocation 1 \
+                -httpheader $httpheaders \
+                -bodyvar html \
+            ] \
+            return_headers \
+            html \
+            responsecode \
+            curlErrorNumber
+
     } else {
         error "qc::http:put must have 1 of -data or -infile specified"
     }
 
     switch $curlErrorNumber {
 	0 {
+            if { $responsecode in $valid_response_codes } {
+	        return [encoding convertfrom [qc::http_encoding $return_headers $html] $html] 
+            }
 	    switch $responsecode {
-		200 { 
-		    # OK
-		    return [encoding convertfrom [qc::http_encoding $return_headers $html] $html] 
-		}
-		404 {return -code error -errorcode CURL "URL NOT FOUND $url"}
-		500 {return -code error -errorcode CURL "SERVER ERROR $url"}
-		default {return -code error -errorcode CURL "RESPONSE $responsecode while contacting $url"}
+		404 {
+                    return \
+                        -code error \
+                        -errorcode CURL \
+                        "URL NOT FOUND $url"
+                    }
+		500 {
+                    return \
+                        -code error \
+                        -errorcode CURL \
+                        "SERVER ERROR $url"
+                }
+		default {
+                    return \
+                        -code error \
+                        -errorcode CURL \
+                        "RESPONSE $responsecode while contacting $url"
+                }
 	    }
 	}
 	28 {
-	    return -code error -errorcode TIMEOUT "Timeout after $timeout seconds trying to contact $url"
+	    return \
+                -code error \
+                -errorcode TIMEOUT \
+                "Timeout after $timeout seconds trying to contact $url"
 	}
 	default {
-	    return -code error -errorcode CURL [curl::easystrerror $curlErrorNumber]
+	    return \
+                -code error \
+                -errorcode CURL \
+                [curl::easystrerror $curlErrorNumber]
 	}
     }
 }
@@ -265,6 +367,7 @@ proc qc::http_header_encoding {dict} {
 proc qc::IANAEncoding2TclEncoding {IANAName} {
     
     switch [string tolower $IANAName] {
+        "ascii"       {return ascii; # non-standard}
         "us-ascii"    {return ascii}
         "utf-8"       {return utf-8}
         "utf-16"      {return unicode; # not sure about this}
@@ -357,7 +460,7 @@ proc qc::http_head {args} {
 	lappend httpheaders [qc::http_header $name $value]
     }
 
-    dict2vars [qc::http_curl -nobody 1 -header 1 -headervar headers -url $url -sslverifypeer 0 -sslverifyhost 0 -timeout $timeout -followlocation 1 -httpheader $httpheaders] headers responsecode curlErrorNumber
+    dict2vars [qc::http_curl -nobody 1 -headervar headers -url $url -sslverifypeer 0 -sslverifyhost 0 -timeout $timeout -followlocation 1 -httpheader $httpheaders] headers responsecode curlErrorNumber
 
     switch $curlErrorNumber {
 	0 {
@@ -386,7 +489,7 @@ proc qc::http_exists {args} {
     args $args -timeout 60 -useragent ? -valid_response_codes {100 200} url
     default useragent "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.7) Gecko/20060909 FreeBSD/i386 Firefox/1.5.0.7"
     #
-    dict2vars [qc::http_curl  -nobody 1 -header 1 -headervar headers -url $url -sslverifypeer 0 -sslverifyhost 0 -timeout $timeout -followlocation 1] responsecode curlErrorNumber
+    dict2vars [qc::http_curl  -nobody 1 -headervar headers -url $url -sslverifypeer 0 -sslverifyhost 0 -timeout $timeout -followlocation 1] responsecode curlErrorNumber
 
     if { [in $valid_response_codes $responsecode] } {
 	return true
@@ -397,14 +500,14 @@ proc qc::http_exists {args} {
 
 proc qc::http_save {args} {
     #| Save the HTTP response to a file.
-    args $args -timeout 60 -headers {} -- url file
+    args $args -timeout 60 -headers {} -sslversion tlsv1 -sslverifypeer 0 -sslverifyhost 0 -- url file
 
     set httpheaders {}
     foreach {name value} $headers {
 	lappend httpheaders [qc::http_header $name $value]
     }
 
-    dict2vars [qc::http_curl -httpheader $httpheaders -timeout $timeout -url $url -file $file -sslverifypeer 0 -sslverifyhost 0] responsecode curlErrorNumber
+    dict2vars [qc::http_curl -httpheader $httpheaders -timeout $timeout -url $url -file $file -sslverifypeer $sslverifypeer -sslverifyhost $sslverifyhost -sslversion $sslversion] responsecode curlErrorNumber
     if { $responsecode != 200 } {
 	file delete $file
     }
@@ -475,4 +578,188 @@ proc qc::http_url_resolve {args} {
 	    return -code error -errorcode CURL [curl::easystrerror $curlErrorNumber]
 	}
     }    
+}
+
+proc qc::http_header_get {header_name} {
+    #| Returns the value for the given header.
+    set headers_set_id [ns_conn headers]
+    if { [ns_set ifind $headers_set_id $header_name] == -1 } {
+        return ""
+    } elseif { [string tolower $header_name] in [qc::http_headers_with_value_as_list] } {
+        # Header may appear more than once so get all values for the header.
+        set header_values {}
+        for {set i 0} {$i < [ns_set size $headers_set_id]} {incr i} {
+            if { [string equal -nocase [ns_set key $headers_set_id $i] $header_name] } {
+                lappend header_values [ns_set value $headers_set_id $i]
+            }
+        }
+        
+        return [join $header_values ","]
+    } else {
+        return [ns_set iget $headers_set_id $header_name]
+    }
+}
+
+proc qc::http_header_parse {header_name header_value} {
+    #| Parses the given header value string into a Tcl structure that is a list of dicts.
+    #| E.G. qc::http_header_parse "application/json;q=0.9,text/xml,text/plain;q=0.8"
+    #|      -> {token application/json params {q 0.9}} {token text/xml params {}} {token text/plain params {q 0.8}}
+    
+    # TODO: Header comments.
+    # TODO: Quoted strings.
+    # TODO: Validation of tokens and params?
+
+    # Get the elements of the header value
+    if { [string tolower $header_name] in [qc::http_headers_with_value_as_list] } {
+        set elements [qc::map {x {string trim $x}} [split $header_value ","]]
+    } else {
+        # TODO: Handle other delimiters for other headers.
+        set elements [list $header_value]
+    }
+    
+    set result {}
+    foreach element $elements {
+        set element_dict [dict create]
+        # Parse all params for this element
+        set params [qc::map {x {string trim $x}} [split $element ";"]]
+        # Most elements begin with a token but there are special cases where the element only consists of params.
+        if { [string tolower $header_name] in [list "cookie" "set-cookie" "strict-transport-security"] } {
+            set token ""
+        } else {
+            set token [lindex $params 0]
+            set params [lrange $params 1 end]
+        }
+
+        # Parse param names and their values
+        set param_dict [dict create]
+        foreach param $params {
+            set list [qc::map {x {string trim $x}} [split $param "="]]
+            set name [lindex $list 0]
+            if { [llength $list]  > 1 } {
+                set value [lindex $list 1]
+            } else {
+                set value ""
+            }
+            dict set param_dict $name $value
+        }
+        dict set element_dict token $token
+        dict set element_dict params $param_dict
+        lappend result $element_dict
+    }
+
+    return $result
+}
+
+proc qc::http_headers_with_value_as_list {} {
+    #| A list of headers whose values are defined as a comma separated list.
+    set headers {}
+    lappend headers accept
+    lappend headers accept-charset
+    lappend headers accept-encoding
+    lappend headers accept-language
+    lappend headers accept-ranges
+    lappend headers allow
+    lappend headers cache-control
+    lappend headers connection
+    lappend headers content-encoding
+    lappend headers content-language
+    lappend headers expect
+    lappend headers if-match
+    lappend headers if-none-match
+    lappend headers pragma
+    lappend headers proxy-authenticate
+    lappend headers te
+    lappend headers trailer
+    lappend headers upgrade
+    lappend headers vary
+    lappend headers via
+    lappend headers warning
+    lappend headers www-authenticate
+    lappend headers x-forwarded-for
+    return $headers
+}
+
+proc qc::http_header_sort_values_by_weight {values} {
+    #| Sorts the header values by weight.
+    if { [llength $values] == 0 } {
+        return [list]
+    }
+    set sorted [list [lindex $values 0]]
+    foreach value [lrange $values 1 end] {
+        if { [dict exists $value params q] } {
+            set weight [dict get $value params q]
+        } else {
+            set weight 1
+        }
+        set inserted false
+        for { set i 0 } { $i < [llength $sorted] } { incr i } {
+            set dict [lindex $sorted $i]
+            if { [dict exists $dict params q] } {
+                set sorted_weight [dict get $dict params q]
+            } else {
+                set sorted_weight 1
+            }
+            if { $weight > $sorted_weight } {
+                set sorted [linsert $sorted $i $value]
+                set inserted true
+                break
+            }
+        }
+        
+        if { ! $inserted } {
+            set sorted [linsert $sorted end $value]
+        }
+    }
+    return $sorted
+}
+
+proc qc::http_accept_header_best_mime_type {available_media} {
+    #| Returns a mime type from the available media list that is the best match
+    #| for the preferences of the Accept header.
+    #| If any mime type will suffice then "*/*" is returned.
+    #| Returns the empty string if no sufficient match is found.
+    
+    # TODO specificity.
+    set sorted [qc::http_header_sort_values_by_weight [qc::http_header_parse Accept [qc::http_header_get Accept]]]
+    if { [llength $sorted] == 0 } {
+        return "*/*"
+    }
+    foreach dict $sorted {
+        lassign [split [dict get $dict token] "/"] type subtype
+        # If best mime type is "*/*" then let caller know so that they can choose the most suitable type.
+        if { $type eq "*" && $subtype eq "*" } {
+            return "$type/$subtype"
+        }
+        foreach item $available_media {
+            lassign [split $item "/"] available_type available_subtype
+            if { [string match -nocase $type $available_type] && [string match -nocase $subtype $available_subtype] } {
+                return $item
+            }
+        }
+    }
+    return ""
+}
+
+proc qc::http_content_negotiate {available_mime_types} {
+    #| Try to find an acceptable mime type to respond to the client with.
+    #| Returns a mime type if an acceptable one was found otherwise the empty string.
+    set result "" 
+    set suffix [string range [file extension [qc::conn_path]] 1 end]
+    # Prioritise suffix type because it is a direct request for a resource of that type.
+    if { $suffix ne "" } {
+        # Check if the suffix type is available and that the client accepts it.
+        foreach mime_type $available_mime_types {
+            lassign [split $mime_type "/"] type subtype
+            if { [string match -nocase $subtype $suffix] && [qc::http_accept_header_best_mime_type [list $mime_type]] ne "" } {
+                set result $mime_type
+                break
+            }
+        }
+    }
+
+    if { $result eq "" } {
+        set result [qc::http_accept_header_best_mime_type $available_mime_types]
+    }
+
+    return $result
 }
