72650755c8efb9f12fb9c8b3977678ac0e16f8ee
David Osborne <david@qcode.co.uk>
2019-06-18 12:10:03 +0100

    no empty namespace prefixes in soap template

--- tcl/soap.tcl
+++ tcl/soap.tcl
@@ -3,16 +3,32 @@ namespace eval qc {
 }
 
 proc qc::soap_template {xml method {namespace ""} } {
-    sset soap {<?xml version="1.0"?>
+    #| SOAP template
+    set soap {<?xml version="1.0"?>
 	<soap:Envelope
 	xmlns:soap="http://www.w3.org/2001/12/soap-envelope"
 	soap:encodingStyle="http://www.w3.org/2001/12/soap-encoding">
 	<soap:Body>
-	<ns:$method xmlns:ns="$namespace">
+	<${ns_prefix}${method}${namespace}>
 	$xml
-	</ns:$method>
+	</${ns_prefix}${method}>
 	</soap:Body>
 	</soap:Envelope>
     }
+    # Namespace prefix binding may not be empty.
+    # If no namespace, do not use prefix
+    set ns_prefix ""
+    if { $namespace ne "" } {
+        set namespace " xmlns:ns=\"$namespace\""
+        set ns_prefix "ns:"
+    }
+    set soap [string map \
+                    [list \${method}       $method \
+                          \$xml            $xml \
+                          \${namespace}    $namespace \
+                          \${ns_prefix}    $ns_prefix \
+                    ] \
+                    $soap \
+             ]
     return $soap
 }
