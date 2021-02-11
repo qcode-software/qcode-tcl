59603a5a4467d161e3c7bad8cbed911e0d931488
Nicky Johnstone <nicky@qcode.co.uk>
2021-02-11 11:49:16 +0000

    Rework image_resize_task APIs

--- tcl/image_resize_task.tcl
+++ tcl/image_resize_task.tcl
@@ -4,14 +4,39 @@ namespace eval qc {
         image_resize_task_process
 }
 
-proc qc::image_resize_task_add {
-    file_id
-    cache_dir
-    width
-    height
-    {autocrop false}
-} {
+proc qc::image_resize_task_add {args} {
     #| Add a new resize task for an image in the database.
+
+    qc::args $args {*}{
+        -autocrop
+        -mime_type "*/*"
+        -cache_dir ""
+        --
+        file_id
+        width
+        height
+    }
+
+    default autocrop false
+
+    if { $cache_dir eq "" } {
+        set cache_dir [ns_pagepath]/image
+    }
+
+    if { $mime_type eq "*/*" } {
+        db_1row {
+            select
+            mime_type
+
+            from
+            file
+
+            where
+            file_id=:file_id
+        }
+    }
+
+    # obtain an advisory lock
 
     db_0or1row {
         select
@@ -25,6 +50,7 @@ proc qc::image_resize_task_add {
         and cache_dir=:cache_dir
         and autocrop=:autocrop
         and task_state = 'QUEUED'
+        and mime_type=:mime_type
         and (
              (
               width=:width
@@ -53,86 +79,10 @@ proc qc::image_resize_task_add {
                 date_added
                 task_state
                 autocrop
+                mime_type
             }]
         "
     }
 
     return $row_id
-}
-
-proc qc::image_resize_task_process { row_id } {
-    #| Resize and cache the image from the task queue.
-
-    db_trans {
-        db_0or1row {
-            select
-            i.file_id,
-            i.cache_dir,
-            i.height,
-            i.width,
-            i.autocrop,
-            f.mime_type
-
-            from
-            image_resize_task i
-            join file f using (file_id)
-
-            where
-            i.row_id=:row_id
-            and i.task_state = 'QUEUED'
-
-            for update
-        } {
-            error "No queued image resize task exists for row ${row_id}."
-        }
-
-        set date_processed [qc::cast timestamptz "now"]
-
-        ::try {
-            set task_state "PROCESSED"
-
-            if { ! [qc::_image_cache_original_exists $cache_dir $file_id] } {
-                # Cache the original image.
-                qc::_image_cache_original_create $cache_dir $file_id
-            }
-
-            if { $mime_type eq "*/*" } {
-                set mime_type [qc::mime_type_guess \
-                                   [qc::_image_cache_original_file \
-                                        $cache_dir $file_id]]
-            }
-
-            if { $mime_type ne "image/svg+xml" } {
-                qc::_image_cache_create \
-                    $cache_dir \
-                    $file_id \
-                    $mime_type \
-                    $width \
-                    $height \
-                    $autocrop
-            }
-
-        } on error [list message options] {
-            set task_state "ERROR"
-
-            error \
-                $message \
-                [dict get $options -errorinfo] \
-                [dict get $options -errorcode]
-        } finally {
-            db_dml "
-                update
-                image_resize_task
-
-                set
-                [qc::sql_set {*}{
-                    task_state
-                    date_processed
-                }]
-
-                where
-                row_id=:row_id
-            "
-        }
-    }
 }
