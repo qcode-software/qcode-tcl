c887e4cbc6189c3db3482873d1332e257b144ab2
David Osborne <david@qcode.co.uk>
2021-06-04 16:33:03 +0100

    Try to get exit code if tests fail

--- test/all.tcl
+++ test/all.tcl
@@ -1,3 +1,11 @@
 package require tcltest
 namespace import ::tcltest::runAllTests
+
+proc tcltest::cleanupTestsHook {} {
+    variable num_tests
+    set ::exit_code [expr {$num_tests(Failed) > 0}]
+}
+
 runAllTests
+
+exit $exit_code
