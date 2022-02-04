77f9bc08cd47997dec8778a7212b6c1243d11475
priyank <priyank@qcode.co.uk>
2022-02-04 09:33:55 +0000

    Updated regexp to include anchor in url

--- tcl/is.tcl
+++ tcl/is.tcl
@@ -981,7 +981,7 @@ namespace eval qc::is {
         # Absolute next url
         # check that redirection is to the same domain
         set conn_host [qc::conn_host]
-        if { ![regexp "^https?://${conn_host}(:\[0-9\]+)?(\\?|/|\$)" $next_url] } {
+        if { ![regexp "^https?://${conn_host}(:\[0-9\]+)?(\\?|#|/|\$)" $next_url] } {
             return 0
         }
         # check for malicious mal-formed url
