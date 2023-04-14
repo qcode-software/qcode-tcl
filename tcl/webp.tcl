7636ce4bac9a842e8292a3fba44abd944108cdce
Nicky Johnstone <nicky@qcode.co.uk>
2023-04-14 17:40:23 +0100

    Remove old qc::is_webp definition

--- tcl/webp.tcl
+++ tcl/webp.tcl
@@ -9,23 +9,12 @@ proc qc::is_webp {file} {
 
     binary scan $chunk0 "a4ia4" riff size id
 
-    if { [info exists riff] && [info exists id] } {
-        return [expr {$riff eq "RIFF" && $id eq "WEBP"}]
-    } else {
-        return 0
-    }
-}
-
-proc qc::is_webp_old {file} {
-    #| Test whether a given file is a webp image
-    set fin [open $file rb]
-    set chunk0 [read $fin 12]
-    set chunk1 [read $fin 28]
-    close $fin
-
-    binary scan $chunk0 "a4ia4" riff size id
-
-    return [expr {$riff eq "RIFF" && $id eq "WEBP"}]
+    return [expr {
+        [info exists riff]
+        && [info exists id]
+        && $riff eq "RIFF"
+        && $id eq "WEBP"
+    }]
 }
 
 proc qc::webpsize {file} {
