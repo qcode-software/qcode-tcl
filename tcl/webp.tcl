7734e11fbb3fc086d35c5ac1834f6255cae55237
Nicky Johnstone <nicky@qcode.co.uk>
2023-04-14 17:31:35 +0100

    Account for small files in qc::is_webp

--- tcl/webp.tcl
+++ tcl/webp.tcl
@@ -5,6 +5,21 @@ proc qc::is_webp {file} {
     #| Test whether a given file is a webp image
     set fin [open $file rb]
     set chunk0 [read $fin 12]
+    close $fin
+
+    binary scan $chunk0 "a4ia4" riff size id
+
+    if { [info exists riff] && [info exists id] } {
+        return [expr {$riff eq "RIFF" && $id eq "WEBP"}]
+    } else {
+        return 0
+    }
+}
+
+proc qc::is_webp_old {file} {
+    #| Test whether a given file is a webp image
+    set fin [open $file rb]
+    set chunk0 [read $fin 12]
     set chunk1 [read $fin 28]
     close $fin
 
