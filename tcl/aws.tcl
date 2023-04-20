cb92d3ed66879302b309067d4a50a9239c9a2412
David Osborne <david@qcode.co.uk>
2023-04-20 17:17:33 +0100

    EoD aws tests

--- tcl/aws.tcl
+++ tcl/aws.tcl
@@ -32,15 +32,25 @@ proc qc::aws_region_set { region } {
 proc qc::aws_credentials_set_from_ec2_role { {region "eu-west-1"} } {
     #| Apply the EC2 role credentials found attached to this EC2 instance
 
-    # Get role credentials
+    # Set region
     qc::aws_region_set $region
+
+    # Set role credentials
+    lassign [qc::aws_credentials_get_from_ec2_role] access_key secret_access_key token
+    qc::aws_credentials_set $access_key $secret_access_key $token
+}
+
+proc qc::aws_credentials_get_from_ec2_role { } {
+    #| Get role credentials from ec2 role metadata
     set role_name [qc::aws_metadata iam/security-credentials/]
     set role_credentials_json [qc::aws_metadata iam/security-credentials/${role_name}]
     set role_credentials_tson [qc::json2tson $role_credentials_json]
     set access_key [qc::tson_object_get_value $role_credentials_tson AccessKeyId]
     set secret_key [qc::tson_object_get_value $role_credentials_tson SecretAccessKey]
     set token [qc::tson_object_get_value $role_credentials_tson Token]
-
-    # Set role credentials
-    qc::aws_credentials_set $access_key $secret_key $token
+    return [list \
+                $access_key \
+                $secret_key \
+                $token \
+            ]
 }
